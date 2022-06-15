952f5b34dca8503622486730eab34ca0b0113468
gz <18260627076@163.com>
2022-06-15 11:47:35 +0800

    pust commit

--- src/main/java/com/atguigu/git/Git0212.java
+++ src/main/java/com/atguigu/git/Git0212.java
@@ -8,6 +8,7 @@ public class Git0212 {
         System.out.println("hello git0214");
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("push test");
 
     }
 }
