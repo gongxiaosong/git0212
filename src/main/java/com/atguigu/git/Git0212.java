5b9040d7ab1d9e260587d80f34df8e858043f930
gz <18260627076@163.com>
2022-06-15 10:31:57 +0800

    second commit

--- src/main/java/com/atguigu/git/Git0212.java
+++ src/main/java/com/atguigu/git/Git0212.java
@@ -2,6 +2,7 @@ package com.atguigu.git;
 
 public class Git0212 {
     public static void main(String[] args) {
+        System.out.println("hello git0212222");
         System.out.println("hello git0212");
 
 
